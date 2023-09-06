b0abe8431b9e9e7c4b60b5fb46204ff418bb4ae8
MrArcano <salvatore-dario@hotmail.it>
2023-09-06 17:09:56 +0200

    [add]: replace space char + rnd number

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,13 @@
-// chiedo e carico i dati
-const userName = prompt("Nome: ");
-const userLastName = prompt("Cognome: ");
-const userColor = prompt("Colore preferito: ");
+// chiedo i dati, elimino gli spazi, e li carico nelle variabili
+const userName = prompt("Nome: ").replace(/ /g,"");
+const userLastName = prompt("Cognome: ").replace(/ /g,"");
+const userColor = prompt("Colore preferito: ").replace(/ /g,"");
+
+// Randomize number 1 to 100
+const nRandom = Math.floor(Math.random() * 100) + 1;
 
 // Compongo la password
-const userPassword = userName + userLastName + userColor + "23"
+const userPassword = userName + userLastName + userColor + nRandom
 
 // Scrivo nella pagina la password
 
