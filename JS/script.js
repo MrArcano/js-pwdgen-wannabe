facc5c61e33cca1e91da457d60c1c7b8d7ff8a9b
MrArcano <salvatore-dario@hotmail.it>
2023-09-06 18:22:34 +0200

    [edit]: randomize number min & max

--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,10 @@ const userName = prompt("Nome: ").replace(/ /g,"");
 const userLastName = prompt("Cognome: ").replace(/ /g,"");
 const userColor = prompt("Colore preferito: ").replace(/ /g,"");
 
-// Randomize number 1 to 100
-const nRandom = Math.floor(Math.random() * 100) + 1;
+// Randomize number min to max
+const min = 10;
+const max = 99;
+const nRandom = Math.floor(Math.random() * (max - min + 1)) + min;
 
 // Compongo la password
 const userPassword = userName + userLastName + userColor + nRandom
